bba1cd309c9f29d8fe43204543f665ab168ec6a9
Peter Hegedus <hegedus21@gmail.com>
2016-11-09 22:09:35 +0100

    Modified Index.cshtml
    Modified Index.cshtml

--- src/MaiXIFY/wwwroot/_references.js
+++ src/MaiXIFY/wwwroot/_references.js
@@ -1,4 +1,5 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="../scripts/handlebars-v4.0.5.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
